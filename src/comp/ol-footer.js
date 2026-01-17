14445179cd579c7a95d09e4a8417d56cb0c05057
Muugii0101 <one.munkhzul@gmail.com>
2026-01-17 13:24:46 +0800

    footer fixed

--- src/comp/ol-footer.js
+++ src/comp/ol-footer.js
@@ -7,15 +7,15 @@ class MainFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="site" role="contentinfo">
         <nav aria-label="Footer links">
-          <a href="#">D\`D,D'D«D,D1 ¥,¥Ÿ¥.DøD1</a>
-          <a href="#">DD_D¯DñD_D3D'D_¥.</a>
-          <a href="#">D›¥Ÿ¥?D¯DøD¬D</a>
+          <a href="#">Бидний тухай</a>
+          <a href="#">Холбоо барих</a>
+          <a href="#">Тусламж</a>
         </nav>
-        <address class="brand-addr" aria-label="DD_D¯DñD_D_">
+        <address class="brand-addr" aria-label="Ollo">
           <img src="images/logo.png" alt="Ollo Logo" class="footer-logo" />
           <span>Ollo</span>
         </address>
-        <small class="copyright">TeamName Ac 2025. D\`O_¥. ¥?¥?¥. ¥.¥Ÿ¥ŸD¯D,Dø¥? ¥.DøD¬D3DøDøD¯DøD3D'¥?DøD«.</small>
+        <small class="copyright">Ollo Ac 2025. Бүх эрх хуулиар хамгаалагдсан.</small>
       </footer>
     `;
   }
